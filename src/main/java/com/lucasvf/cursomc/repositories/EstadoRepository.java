c0cc8ca7cccfe0b3f2f0546b5f82a10936605e87
Ruivonks <lucas.vfabricio@gmail.com>
2021-05-21 14:13:51 -0300

    Estado e Cidade

---
+++ src/main/java/com/lucasvf/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.lucasvf.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasvf.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+	
+	
+}
