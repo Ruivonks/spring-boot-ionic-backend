5505cddde0a43050c11724e3ee82cc4192b74d19
Ruivonks <lucas.vfabricio@gmail.com>
2021-05-24 13:15:40 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/lucasvf/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,13 @@
+package com.lucasvf.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucasvf.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+	
+	
+}
